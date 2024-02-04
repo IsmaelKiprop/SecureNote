e9547f148115de077ec555130267912492a7d163
IsmaelKiprop <kipropismael27@gmail.com>
2024-02-04 21:51:06 +0300

    updated

--- secure-note-frontend/src/App.js
+++ secure-note-frontend/src/App.js
@@ -1,16 +1,29 @@
-// App.js
+import {
+  HashRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 import './App.css';
 import Header from './components/Header'
-import NotesListPage from './pages/NotesListPage';
+import NotesListPage from './pages/NotesListPage'
+import NotePage from './pages/NotePage'
 
 function App() {
   return (
-    <div className="App"> 
-    <Header/> 
-    <NotesListPage/>
-    </div>
-  )
+    <Router>
+      <div className="container maroon"> {/* Added 'maroon' class here */}
+        <div className="app">
+          <Header />
+          <Routes>
+            <Route path="/" element={<NotesListPage />} />
+            <Route path="/note/:id" element={<NotePage />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
 
--- secure-note-frontend/src/components/Header.js
+++ secure-note-frontend/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      <hi>Our Header</hi>
+      <hi></hi>
     </div>
   )
 }
