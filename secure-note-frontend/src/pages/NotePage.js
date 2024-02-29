95da7d554876412742f216b369fed8cb1f419bc2
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-02-29 13:51:25 +0300

    Update NotePage.js

--- secure-note-frontend/src/pages/NotePage.js
+++ secure-note-frontend/src/pages/NotePage.js
@@ -1,73 +1,65 @@
-import React, { useState, useEffect } from 'react'
-import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg';
 
 const NotePage = ({ match, history }) => {
-
-    let noteId = match.params.id
-    let [note, setNote] = useState(null)
+    const noteId = match.params.id;
+    const [note, setNote] = useState(null);
 
     useEffect(() => {
-        getNote()
-    }, [noteId])
+        getNote();
+    }, [getNote, noteId]);
 
+    const getNote = async () => {
+        if (noteId === 'new') return;
+        const response = await fetch(`/api/notes/${noteId}/`);
+        const data = await response.json();
+        setNote(data);
+    };
 
-    let getNote = async () => {
-        if (noteId === 'new') return
-
-        let response = await fetch(`/api/notes/${noteId}/`)
-        let data = await response.json()
-        setNote(data)
-    }
-
-    let createNote = async () => {
-        fetch(`/api/notes/`, {
+    const createNote = async () => {
+        await fetch(`/api/notes/`, {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(note)
-        })
-    }
+        });
+    };
 
-
-    let updateNote = async () => {
-        fetch(`/api/notes/${noteId}/`, {
+    const updateNote = async () => {
+        await fetch(`/api/notes/${noteId}/`, {
             method: "PUT",
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(note)
-        })
-    }
+        });
+    };
 
-
-    let deleteNote = async () => {
-        fetch(`/api/notes/${noteId}/`, {
+    const deleteNote = async () => {
+        await fetch(`/api/notes/${noteId}/`, {
             method: 'DELETE',
-            'headers': {
+            headers: {
                 'Content-Type': 'application/json'
             }
-        })
-        history.push('/')
-    }
+        });
+        history.push('/');
+    };
 
-    let handleSubmit = () => {
-        console.log('NOTE:', note)
-        if (noteId !== 'new' && note.body == '') {
-            deleteNote()
+    const handleSubmit = () => {
+        if (noteId !== 'new' && note.body === '') {
+            deleteNote();
         } else if (noteId !== 'new') {
-            updateNote()
+            updateNote();
         } else if (noteId === 'new' && note.body !== null) {
-            createNote()
+            createNote();
         }
-        history.push('/')
-    }
+        history.push('/');
+    };
 
-    let handleChange = (value) => {
-        setNote(note => ({ ...note, 'body': value }))
-        console.log('Handle Change:', note)
-    }
+    const handleChange = (value) => {
+        setNote(prevNote => ({ ...prevNote, 'body': value }));
+    };
 
     return (
         <div className="note" >
@@ -80,11 +72,10 @@ const NotePage = ({ match, history }) => {
                 ) : (
                     <button onClick={handleSubmit}>Done</button>
                 )}
-
             </div>
-            <textarea onChange={(e) => { handleChange(e.target.value) }} value={note?.body}></textarea>
+            <textarea onChange={(e) => handleChange(e.target.value)} value={note?.body}></textarea>
         </div>
-    )
-}
+    );
+};
 
-export default NotePage
+export default NotePage;
