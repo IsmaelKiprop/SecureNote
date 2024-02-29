77088d40195eb0405759bcaac742ee433516a7e6
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-02-29 18:37:50 +0300

    Update App.js

--- secure-note-frontend/src/App.js
+++ secure-note-frontend/src/App.js
@@ -1,23 +1,27 @@
 import {
-  HashRouter as Router,
+  BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
 
 import './App.css';
-import Header from './components/Header'
-import NotesListPage from './pages/NotesListPage'
-import NotePage from './pages/NotePage'
+import Header from './components/Header';
+import NotesListPage from './pages/NotesListPage';
+import NotePage from './pages/NotePage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
     <Router>
-      <div className="container maroon"> {/* Added 'maroon' class here */}
+      <div className="container maroon">
         <div className="app">
           <Header />
           <Routes>
             <Route path="/" element={<NotesListPage />} />
             <Route path="/note/:id" element={<NotePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
           </Routes>
         </div>
       </div>
@@ -26,4 +30,3 @@ function App() {
 }
 
 export default App;
-
