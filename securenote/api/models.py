b3d7d9859af8c900195f9be23c170b21b80f2fa3
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-02-29 19:08:46 +0300

    Update models.py

--- securenote/api/models.py
+++ securenote/api/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Note(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     body = models.TextField(null=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
