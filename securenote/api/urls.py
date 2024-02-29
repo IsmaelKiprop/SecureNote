490788de37a1a84e64e45889fe5b8823a14d863e
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-02-29 17:03:26 +0300

    Update urls.py

--- securenote/api/urls.py
+++ securenote/api/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('notes/create/', views.createNote, name="create-note"),
     path('notes/<str:pk>/update/', views.updateNote, name="update-note"),
     path('notes/<str:pk>/delete/', views.deleteNote, name="delete-note"),
-
     path('notes/<str:pk>/', views.getNote, name="note"),
+    path('signup/', views.signup, name="signup"),
+    path('login/', views.login, name="login"),
 ]
