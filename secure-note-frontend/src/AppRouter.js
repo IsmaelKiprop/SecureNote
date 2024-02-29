f32ce7a2c16fd2599ce6f080b705289a86405ac0
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-02-29 14:00:11 +0300

    Update AppRouter.js

--- secure-note-frontend/src/AppRouter.js
+++ secure-note-frontend/src/AppRouter.js
@@ -1,6 +1,6 @@
 // src/AppRouter.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Change here
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Notes from './components/Notes';
@@ -9,12 +9,12 @@ import NoteDetail from './components/NoteDetail';
 const AppRouter = () => {
   return (
     <Router>
-      <Routes> {/* Change here */}
+      <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/notes" element={<Notes />} />
         <Route path="/note/:id" element={<NoteDetail />} />
-      </Routes> {/* Change here */}
+      </Routes>
     </Router>
   );
 };
