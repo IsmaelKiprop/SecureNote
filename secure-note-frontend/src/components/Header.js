b003454d64df6a3902b1488183bbba756fd33e22
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-03-02 13:39:26 +0300

    Update Header.js

--- secure-note-frontend/src/components/Header.js
+++ secure-note-frontend/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './App.css';
+import './App.css'; // Importing App.css here
 
 const Header = () => {
   const handleLogout = () => {
