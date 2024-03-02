483776b08f78067d2c322a164171de529d8767d3
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-03-02 14:46:44 +0300

    Update LoginPage.js

--- secure-note-frontend/src/pages/LoginPage.js
+++ secure-note-frontend/src/pages/LoginPage.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import '../App.css'; // Import App.css for styling
+import '../App.css'; // Importing App.css here
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
 
   const handleLogin = async () => {
     try {
@@ -14,18 +13,19 @@ const LoginPage = () => {
         password
       });
       const token = response.data.token;
+      // Store token in local storage or state for authentication
       localStorage.setItem('token', token);
-      window.location.href = '/'; // Redirect upon successful login
+      // Redirect to notes list page or any other desired destination
+      window.location.href = '/';
     } catch (error) {
       console.error('Login failed:', error);
-      setErrorMessage('Invalid username or password'); // Set error message if login fails
+      // Handle login error (display error message, clear input fields, etc.)
     }
   };
 
   return (
     <div className="login-page"> {/* Apply class for page styling */}
       <h2>Login</h2>
-      {errorMessage && <p className="error-message">{errorMessage}</p>} {/* Display error message if exists */}
       <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} className="input-field" /> {/* Apply class for input field styling */}
       <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className="input-field" /> {/* Apply class for input field styling */}
       <button onClick={handleLogin} className="login-button">Login</button> {/* Apply class for button styling */}
