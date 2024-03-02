6bb0f11c1b0ee0364e3a33b44e606e0dd61eac5f
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-03-02 15:23:58 +0300

    Update Header.js

--- secure-note-frontend/src/components/Header.js
+++ secure-note-frontend/src/components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css'; // Importing App.css here
+// import logo from '../logo.png'; // Importing the logo image
 
 const Header = () => {
+  const isAuthenticated = localStorage.getItem('token'); // Check if the user is authenticated
+
   const handleLogout = () => {
     localStorage.removeItem('token'); // Remove authentication token from local storage
     window.location.href = '/login'; // Redirect to the login page after logout
@@ -10,13 +13,12 @@ const Header = () => {
 
   return (
     <header className="app-header">
+      <img src={logo} alt="Logo" className="app-logo" /> {/* Logo image */}
       <h1 className="app-title" style={{ color: '#800000' }}>GuardianNote</h1> {/* Application name with maroon color */}
       <nav>
         <ul>
           <li><Link to="/" className="nav-link">Home</Link></li>
-          <li><Link to="/signup" className="nav-link">Sign Up</Link></li>
-          <li><Link to="/login" className="nav-link">Login</Link></li>
-          <li><button onClick={handleLogout} className="logout-button">Logout</button></li>
+          {isAuthenticated && <li><button onClick={handleLogout} className="logout-button">Logout</button></li>}
         </ul>
       </nav>
     </header>
