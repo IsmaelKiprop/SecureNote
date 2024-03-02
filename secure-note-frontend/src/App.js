de102b4c306c97e39bbd8a4616440a755e14d908
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-03-02 12:42:53 +0300

    Update App.js

--- secure-note-frontend/src/App.js
+++ secure-note-frontend/src/App.js
@@ -11,20 +11,20 @@ import NotesListPage from './pages/NotesListPage';
 import NotePage from './pages/NotePage';
 import SignupPage from './pages/SignupPage';
 import LoginPage from './pages/LoginPage';
+import LandingPage from './pages/LandingPage'; // Import LandingPage component
 
 function App() {
   return (
     <Router>
-      <div className="container maroon">
-        <div className="app">
-          <Header />
-          <Routes>
-            <Route path="/" element={<NotesListPage />} />
-            <Route path="/note/:id" element={<NotePage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/login" element={<LoginPage />} />
-          </Routes>
-        </div>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" element={<LandingPage />} /> {/* Route for landing page */}
+          <Route path="/notes" element={<NotesListPage />} />
+          <Route path="/note/:id" element={<NotePage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
       </div>
     </Router>
   );
