5f476898ee58ef7f9fa70cd6ea712e274948052e
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-03-02 16:16:37 +0300

    Update Header.js

--- secure-note-frontend/src/components/Header.js
+++ secure-note-frontend/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css'; // Importing App.css here
+import logo from '../assets/logo.png'; // Importing the local logo image
 
 const Header = () => {
   const isAuthenticated = localStorage.getItem('token'); // Check if the user is authenticated
@@ -12,11 +13,7 @@ const Header = () => {
 
   return (
     <header className="app-header">
-      <div dangerouslySetInnerHTML={{ __html: `
-        <div>
-          <img src='https://private-user-images.githubusercontent.com/133222922/302182174-1a409a86-25ab-410e-af84-88e8947a622e.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDkzODQzNjgsIm5iZiI6MTcwOTM4NDA2OCwicGF0aCI6Ii8xMzMyMjI5MjIvMzAyMTgyMTc0LTFhNDA5YTg2LTI1YWItNDEwZS1hZjg0LTg4ZTg5NDdhNjIyZS5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwMzAyJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDMwMlQxMjU0MjhaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT04NmI0NDQ5YTRhN2ViMGNjYmViZWQ0NDM0MmU3ZDgyNzEwMGY3ZjczYjAwYjY3NTYzZjZkZmU1YTA0OGNmZTQ2JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.MTgUEQZIf4JnKSo_HnNpxOuewkORQH4wvmVowK_VDN0>'
-        </div>
-      `}} />
+      <img src={logo} alt="Logo" className="app-logo" /> {/* Local logo image */}
       <h1 className="app-title" style={{ color: '#800000' }}>GuardianNote</h1> {/* Application name with maroon color */}
       <nav>
         <ul>
@@ -28,4 +25,4 @@ const Header = () => {
   );
 }
 
-export default Header;
+export default Header
