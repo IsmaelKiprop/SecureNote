57440a6d2aef2e8f0428341813d3afbae93aec38
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-03-03 22:39:27 +0300

    Update LandingPage.js

--- secure-note-frontend/src/pages/LandingPage.js
+++ secure-note-frontend/src/pages/LandingPage.js
@@ -8,14 +8,9 @@ const LandingPage = () => {
             <h1>Welcome to GuardianNote</h1>
             <p>A secure note-taking application</p>
             <p>GuardianNote prioritizes the protection of sensitive information, providing a robust and secure platform for individuals to store their confidential notes with confidence.</p>
-            <p>Key features include:</p>
-            <ul>
-                <li>Secure storage of confidential notes</li>
-                <li>User-friendly interface for easy note management</li>
-                <li>Token-based authentication for enhanced security</li>
-                <li>Responsive design for seamless use on various devices</li>
-            </ul>
+    
             <p>Get started today and keep your notes safe and secure with GuardianNote!</p>
+
             <div className="landing-buttons">
                 <Link to="/login" className="btn">Login</Link>
                 <Link to="/signup" className="btn">Sign Up</Link>
