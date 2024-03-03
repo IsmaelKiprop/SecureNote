191e8b0fd9fc30f1bcd2dba7f5eb959cce97c16d
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-03-03 23:54:53 +0300

    Update LandingPage.js

--- secure-note-frontend/src/pages/LandingPage.js
+++ secure-note-frontend/src/pages/LandingPage.js
@@ -4,16 +4,18 @@ import '../App.css'; // Importing App.css here
 
 const LandingPage = () => {
     return (
-        <div className="landing-page">
-            <h1>Welcome to GuardianNote</h1>
-            <p>A secure note-taking application</p>
-            <p>GuardianNote prioritizes the protection of sensitive information, providing a robust and secure platform for individuals to store their confidential notes with confidence.</p>
-    
-            <p>Get started today and keep your notes safe and secure with GuardianNote!</p>
+        <div className="landing-page-container">
+            <div className="landing-page">
+                <h1>Welcome to GuardianNote</h1>
+                <p>A secure note-taking application</p>
+                <p>GuardianNote prioritizes the protection of sensitive information, providing a robust and secure platform for individuals to store their confidential notes with confidence.</p>
+        
+                <p>Get started today and keep your notes safe and secure with GuardianNote!</p>
 
-            <div className="landing-buttons">
-                <Link to="/login" className="btn">Login</Link>
-                <Link to="/signup" className="btn">Sign Up</Link>
+                <div className="landing-buttons">
+                    <Link to="/login" className="btn">Login</Link>
+                    <Link to="/signup" className="btn">Sign Up</Link>
+                </div>
             </div>
             <footer className="footer">
                 <p>Copyright © {new Date().getFullYear()} GuardianNote. All rights reserved.</p>
