f692bb7ac2d7f21b7c1fcc9e0f288a9bf70c3681
Ismael <133222922+IsmaelKiprop@users.noreply.github.com>
2024-03-05 07:45:45 +0300

    Update settings.py

--- securenote/securenote/settings.py
+++ securenote/securenote/settings.py
@@ -39,10 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "api.apps.ApiConfig",
     'rest_framework',
-    'rest_framework.authtoken'
-    'corsheaders',
-
-
+    'rest_framework.authtoken',  # Correct module name
+    'corsheaders',  # Add a comma here
 ]
 
 MIDDLEWARE = [
